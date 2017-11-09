a71d773797afc3a3625114636a615238181fa0a1
BasvRossem <31800724+BasvRossem@users.noreply.github.com>
2017-11-10 00:36:11 +0100

    Add files via upload

--- server_Classes.py
+++ server_Classes.py
@@ -2,22 +2,27 @@ import threading
 import socket
 import pickle
 import time
-import pygame
+import pygame, math
+from random import randrange
 
 class SendPacket(object):
-    def __init__(self,X,Y,HP):
+    def __init__(self,X,Y,HP,bullets=list()):
         self.x = X
         self.hp = HP
         self.y = Y
+        self.bullets = bullets
         self.others = dict()
 
 class Player(object):
     def __init__(self,IP):
         self.IP = IP
-        self.player_tank = Tank()
+        self.dead = False
     def Set_Coords(self,Coords):
-        self.player_tank.x += Coords[0]
-        self.player_tank.y += Coords[1]
+        self.player_tank.x += Coords[0] * self.player_tank.spd
+        self.player_tank.y += Coords[1] * self.player_tank.spd
+        self.player_tank.Update_spr()
+    def Create_Tank(self,x,y,Health,Speed,Firerate,Bullet_Damage,player_name):
+        self.player_tank = Tank(x,y,Health,Speed,Firerate,Bullet_Damage,player_name)
 
 class Serverconn(object):
     def __init__(self,Binidng_IP):
@@ -28,7 +33,49 @@ class Serverconn(object):
         self.s.bind((self.IP, self.TCP))
 
 class Tank(object):
-    pass
+    def __init__(self,x,y,Health,Speed,Firerate,Bullet_Damage,player_name):
+        self.name = player_name
+        self.x = x
+        self.y = y
+        self.hp = Health
+        self.spd = Speed
+        self.fr = Firerate
+        self.bd = Bullet_Damage
+        self.bullets = []
+        self.sprite = pygame.Rect((self.x,self.y,32,32))
+    def Fire(self,tar_x,tar_y):
+        _bullet = Bullet(self.x,self.y,tar_x,tar_y,self.spd)
+        self.bullets.append(_bullet)
+    def Update_spr(self):
+        self.sprite = pygame.Rect((self.x, self.y, 32, 32))
+
+class Bullet(object):
+    def __init__(self,x,y,tar_x,tar_y,speed):
+        self.x = x
+        self.y = y
+        self.tar_x = tar_x
+        self.tar_y = tar_y
+        self.speed = speed
+        self.active = True
+        delta_x = -(self.x - self.tar_x)
+        delta_y = -(self.y - self.tar_y)
+        self.angle= math.atan2(delta_y,delta_x)
+        self.bulletspr = pygame.Rect((self.x + 5, self.y + 5, 10, 10))
+        # self.angle = math.degrees(angle_rad)
+
+    def Handle(self):
+        self.bulletspr = pygame.Rect((self.x + 5, self.y + 5, 10, 10))
+        if self.active == True:
+            _x = math.sin(self.angle) * 6
+            _y = math.cos(self.angle) * 6
+            self.x += _y
+            self.y += _x
+        else:
+            del self
+    def Draw(self,screen):
+        self.bulletspr = pygame.Rect((self.x+5, self.y+5,10,10))
+        bullettimage = pygame.image.load("res/Bullet.png")
+        screen.blit(bullettimage, (self.x+5, self.y+5))
 
 class Socket_Listen_Thread(threading.Thread):
     def __init__(self,serverconn):
@@ -46,14 +93,15 @@ class Socket_Listen_Thread(threading.Thread):
                     break
                 except:
                     pass
-            if (packet.text != "FIRE"):
-                p.Set_Coords(self.action[packet.text])
-            else:
-                pass
+            if (p.dead == False):
+                if (packet.text != "FIRE"):
+                    p.Set_Coords(self.action[packet.text])
+                else:
+                    p.player_tank.Fire(packet.x,packet.y)
 
         adresses = []
         self.players = []
-        thread_count = 0
+        self.thread_count = 0
 
         def checku():
             adresses.append(addr)
@@ -64,7 +112,9 @@ class Socket_Listen_Thread(threading.Thread):
                     break
                 except:
                     pass
-            p.player_tank = packet
+            x = randrange(0,800)
+            y = randrange(0,800)
+            p.Create_Tank(x,y,packet.hp,packet.spd,packet.fr,packet.bd,packet.name)
             self.players.append(p)
 
         while 1:
@@ -74,9 +124,9 @@ class Socket_Listen_Thread(threading.Thread):
                     if p.IP == addr:
                         threading.Thread(target=check(p))
             else:
-                if thread_count < 4:
+                if self.thread_count < 4:
                     threading.Thread(target=checku())
-                    thread_count+=1
+                    self.thread_count+=1
 
 class Connection_Listen_Thread(threading.Thread):
     def __init__(self,thread_ID,addr):
